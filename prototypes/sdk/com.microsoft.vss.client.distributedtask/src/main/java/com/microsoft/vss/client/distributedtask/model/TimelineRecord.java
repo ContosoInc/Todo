7e3c1718257b785a6fcacd1e1e11a8d379026969
Alex Rukhlin <arukhlin@microsoft.com>
2015-01-29 17:19:33 -0500

    Light refactoring

--- prototypes/sdk/com.microsoft.vss.client.distributedtask/src/main/java/com/microsoft/vss/client/distributedtask/model/TimelineRecord.java
+++ prototypes/sdk/com.microsoft.vss.client.distributedtask/src/main/java/com/microsoft/vss/client/distributedtask/model/TimelineRecord.java
@@ -10,27 +10,196 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.microsoft.vss.client.distributedtask.model.enumeration.TimelineRecordState;
 
 public class TimelineRecord {
-    public UUID id;
-    public UUID parentId;
+    private UUID id;
+    private UUID parentId;
+    private String recordType;
+    private String name;
+    private Date startTime;
+    private Date finishTime;
+    private String currentOperation;
+    private int percentComplete;
+    private TimelineRecordState state;
+    private TaskResult result;
+    private String resultCode;
+    private int changeId;
+    private Date lastModified;
+    private String workerName;
+    private int order;
+    private TaskLogReference log;
+    private TimelineReference details;
+    private int errorCount;
+    private int warningCount;
+    private List<Issue> issues = new ArrayList<Issue>();
+    private URI location;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(final UUID id) {
+        this.id = id;
+    }
+
+    public UUID getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(final UUID parentId) {
+        this.parentId = parentId;
+    }
+
     @JsonProperty("type")
-    public String recordType;
-    public String name;
-    public Date startTime;
-    public Date finishTime;
-    public String currentOperation;
-    public int percentComplete;
-    public TimelineRecordState state;
-    public TaskResult result;
-    public String resultCode;
-    public int changeId;
-    public Date lastModified;
-    public String workerName;
-    public int order;
-    public TaskLogReference log;
-    public TimelineReference details;
-    public int errorCount;
-    public int warningCount;
-    public List<Issue> issues = new ArrayList<Issue>();
-    public URI location;
+    public String getRecordType() {
+        return recordType;
+    }
+
+    @JsonProperty("type")
+    public void setRecordType(final String recordType) {
+        this.recordType = recordType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(final Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getFinishTime() {
+        return finishTime;
+    }
+
+    public void setFinishTime(final Date finishTime) {
+        this.finishTime = finishTime;
+    }
+
+    public String getCurrentOperation() {
+        return currentOperation;
+    }
+
+    public void setCurrentOperation(final String currentOperation) {
+        this.currentOperation = currentOperation;
+    }
+
+    public int getPercentComplete() {
+        return percentComplete;
+    }
+
+    public void setPercentComplete(final int percentComplete) {
+        this.percentComplete = percentComplete;
+    }
+
+    public TimelineRecordState getState() {
+        return state;
+    }
+
+    public void setState(final TimelineRecordState state) {
+        this.state = state;
+    }
+
+    public TaskResult getResult() {
+        return result;
+    }
+
+    public void setResult(final TaskResult result) {
+        this.result = result;
+    }
+
+    public String getResultCode() {
+        return resultCode;
+    }
+
+    public void setResultCode(final String resultCode) {
+        this.resultCode = resultCode;
+    }
+
+    public int getChangeId() {
+        return changeId;
+    }
+
+    public void setChangeId(final int changeId) {
+        this.changeId = changeId;
+    }
+
+    public Date getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(final Date lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public String getWorkerName() {
+        return workerName;
+    }
+
+    public void setWorkerName(final String workerName) {
+        this.workerName = workerName;
+    }
+
+    public int getOrder() {
+        return order;
+    }
+
+    public void setOrder(final int order) {
+        this.order = order;
+    }
+
+    public TaskLogReference getLog() {
+        return log;
+    }
+
+    public void setLog(final TaskLogReference log) {
+        this.log = log;
+    }
+
+    public TimelineReference getDetails() {
+        return details;
+    }
+
+    public void setDetails(final TimelineReference details) {
+        this.details = details;
+    }
+
+    public int getErrorCount() {
+        return errorCount;
+    }
+
+    public void setErrorCount(final int errorCount) {
+        this.errorCount = errorCount;
+    }
+
+    public int getWarningCount() {
+        return warningCount;
+    }
+
+    public void setWarningCount(final int warningCount) {
+        this.warningCount = warningCount;
+    }
+
+    public List<Issue> getIssues() {
+        return issues;
+    }
+
+    public void setIssues(final List<Issue> issues) {
+        this.issues = issues;
+    }
+
+    public URI getLocation() {
+        return location;
+    }
+
+    public void setLocation(final URI location) {
+        this.location = location;
+    }
 
 }
